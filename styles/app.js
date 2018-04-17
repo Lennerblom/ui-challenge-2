5cf34f238c3dffd6b27b366b0e7110adf48864af
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-04-16 21:31:57 -0700

    just beginnig still, will need to resubmit

---
+++ styles/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let dDownOdd = $('opOdd').css({
+  'background-color': '#555',
+  'text-align': 'center'
+});
+dDownOdd;
+
+let h1 = $('title').css({
+  'background-color': '#555',
+  'text-align': 'center'
+});
+h1;
+console.log(h1);
